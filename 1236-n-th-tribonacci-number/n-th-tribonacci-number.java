5972b1a1dc77294aa5a5c498f5b112b38a5506e8
R Abishek <168850170+Abishek7952@users.noreply.github.com>
2025-07-31 20:40:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (9.66%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public int tribonacci(int n){
+        int[] arr=new int[n+1];
+        Arrays.fill(arr,-1);
+        return tribosum(n,arr);
+    }
+    public int tribosum(int n,int[] arr) {
+        if(n<2){
+            return n;
+        }
+        if(n==2){
+            return 1 ;
+        }
+        if(arr[n]!=-1){
+            return arr[n];
+        }
+
+       arr[n]=tribosum(n-1,arr)+tribosum(n-2,arr)+tribosum(n-3,arr);
+      return arr[n];
+    }
+}
