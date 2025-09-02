fe6b7941eae5302fdf0d0b4e9dd2da5e5d942e06
R Abishek <168850170+Abishek7952@users.noreply.github.com>
2025-09-03 01:13:34 +0530

    Time: 14 ms (31.25%) | Memory: 45.3 MB (19.41%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,12 +1,16 @@
+
+// NOT OPTIMIZED
 class Solution {
     public boolean isPalindrome(String s) {
-        
+        // CONV TO LOWER CASER
         s=s.toLowerCase();
+
+        // TO REMOVE SYMBOLS 
         s=s.replaceAll("[^a-z0-9]","");
 
         int start=0;
         int end=s.length()-1;
-
+        // PALINDROME CHECK
         while(start<=end){
             if(s.charAt(start)!=s.charAt(end)){
                 return false;
